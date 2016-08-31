07416260806688b10c04aa1e5e9e3848fe84776d
Blesson Rajan Babu <blessonrajan@users.noreply.github.com>
2016-09-01 02:59:51 +0530

    Baseline for first release (#1)
    added rb-uib-multiselect directive
    set version to 1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp')
+var pump = require('pump')
+var eslint = require('gulp-eslint')
+var header = require('gulp-header')
+var uglify = require('gulp-uglify')
+var rename = require('gulp-rename')
+var watch = require('gulp-watch')
+
+gulp.task('lint', function () {
+  return gulp.src('src/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError())
+})
+
+gulp.task('build', function () {
+
+  var pkg = require('./package.json')
+  var banner = [
+    '/**',
+    ' * <%= pkg.name %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @link <%= pkg.homepage %>',
+    ' * @license <%= pkg.license %>',
+    ' */',
+    ''].join('\n')
+
+  pump([
+    gulp.src('src/*.js'),
+    header(banner, { pkg: pkg }),
+    gulp.dest('dist')
+  ])
+
+  pump([
+    gulp.src('src/*.js'),
+    uglify(),
+    header(banner, { pkg: pkg }),
+    rename({ suffix: '.min' }),
+    gulp.dest('dist')
+  ])
+})
+
+gulp.task('watch', function () {
+  return watch('src/*.js')
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('default', ['lint', 'build'])
---
+++ src/rb-uib-multiselect.js
@@ -0,0 +1,148 @@
+'use strict'
+
+angular.module('rb-uib-multiselect', ['ui.bootstrap'])
+
+angular.module('rb-uib-multiselect').directive('rbUibMultiselect', function () {
+  return {
+    restrict: 'AE',
+    scope: {
+      inputOptions: '=',
+      outputOptions: '=',
+      maxLabels: '@',
+      buttonClass: '@',
+      menuClass: '@',
+      selectText: '@',
+      selectAllText: '@',
+      clearAllText: '@',
+      itemLabel: '&',
+      buttonLabel: '&'
+    },
+    templateUrl: 'rb-uib-multiselect.html',
+    link: function (scope, element, attrs) {
+      // reset input, output
+      scope.reset = function () {
+        scope.options = []
+        scope.outputOptions = []
+        scope.selectAll = false
+      }
+      scope.reset()
+
+      // set defaults
+      scope.setDefaults = function () {
+        if (scope.maxLabels === undefined) {
+          scope.maxLabels = scope.options.length
+        }
+        scope._itemLabel = scope.itemLabel()
+        if (scope._itemLabel === undefined) {
+          scope._itemLabel = function (item) {
+            return ' ' + item.label
+          }
+        }
+        scope._buttonLabel = scope.buttonLabel()
+        if (scope._buttonLabel === undefined) {
+          scope._buttonLabel = function () {
+            if (scope.outputOptions.length === 0) {
+              return scope.selectText
+            }
+            if (scope.maxLabels < scope.outputOptions.length) {
+              return '(' + scope.outputOptions.length + ') Selected'
+            }
+            var labels = []
+            angular.forEach(scope.outputOptions, function (o) {
+              labels.push(scope._itemLabel(o))
+            })
+            return labels.join(', ')
+          }
+        }
+        if (scope.selectText === undefined) {
+          scope.selectText = 'Select'
+        }
+        if (scope.selectAllText === undefined) {
+          scope.selectAllText = ' Select All'
+        }
+        if (scope.clearAllText === undefined) {
+          scope.clearAllText = ' Clear All'
+        }
+      }
+      scope.setDefaults()
+
+      // prepares input options from input model
+      scope.prepareOptions = function () {
+        scope.reset()
+        angular.forEach(scope.inputOptions, function (o) {
+          scope.options.push({ id: o.id, label: scope._itemLabel(o), selected: o.selected || false, source: o })
+        })
+        scope.updateOutput()
+      }
+
+      // watch for input changes
+      scope.$watch('inputOptions', function (newInput) {
+        if (newInput) {
+          scope.prepareOptions()
+        }
+      })
+
+      // on menu item click
+      scope.onItemClick = function (o, $event) {
+        $event.preventDefault()
+        o.selected = !o.selected
+        scope.updateOutput()
+      }
+
+      // on menu item checkbox change
+      scope.onItemSelectionChange = function () {
+        scope.updateOutput()
+      }
+
+      // on select-all click
+      scope.onSelectAllClick = function ($event) {
+        $event.preventDefault()
+        scope.selectAll = !scope.selectAll
+        angular.forEach(scope.options, function (o) {
+          o.selected = scope.selectAll
+        })
+        scope.updateOutput()
+      }
+
+      // updates output model
+      scope.updateOutput = function () {
+        var output = []
+        var allSelected = true
+        angular.forEach(scope.options, function (o) {
+          if (o.selected === true) {
+            output.push(o.source)
+          } else {
+            allSelected = false
+          }
+        })
+        scope.selectAll = allSelected
+        scope.outputOptions = output
+      }
+    }
+  }
+})
+
+angular.module('rb-uib-multiselect').run(['$templateCache', function ($templateCache) {
+  var template = '' +
+  '<div class="btn-group btn-block" uib-dropdown auto-close="outsideClick">' +
+    '<button type="button" class="btn btn-block" uib-dropdown-toggle ng-class="[buttonClass]">' +
+      '{{ _buttonLabel() }}' +
+    '</button>' +
+    '<ul class="dropdown-menu" style="width: 100%;" uib-dropdown-menu role="menu" ng-class="[menuClass]">' +
+      '<li role="menuitem" ng-if="options.length > 0">' +
+        '<a href="#" ng-click="onSelectAllClick($event)">' +
+          '{{ selectAll ? clearAllText : selectAllText }}' +
+        '</a>' +
+      '</li>' +
+      '<li class="divider" ng-if="options.length > 0"></li>' +
+      '<li role="menuitem" ng-repeat="o in options">' +
+          '<a href="#" ng-click="onItemClick(o, $event)">' +
+            '<input type="checkbox" ng-model="o.selected" ng-change="onItemSelectionChange()" ng-click="$event.stopPropagation()">' +
+            '{{ o.label }}' +
+          '</a>' +
+      '</li>' +
+    '</ul>' +
+  '</div>'
+
+  $templateCache.put('rb-uib-multiselect.html', template)
+}])
